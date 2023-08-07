2f51425680525cd3cc5ae02c6cc89314dad4f101
Aadarsh <aadarsh9811594152@gmail.com>
2023-08-07 18:49:58 +0530

    learnig mysql of django

--- my_project/settings.py
+++ my_project/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
-
+git config pull.ff only    
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
